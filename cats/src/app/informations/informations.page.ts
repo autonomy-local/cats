58e5afbf586e36839d80d4aaed691000c8a262ae
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-04-13 14:58:27 +0900

    fix: dummy data

--- cats/src/app/informations/informations.page.ts
+++ cats/src/app/informations/informations.page.ts
@@ -16,25 +16,25 @@ export class InformationsPage implements OnInit {
   informations = [
     {
       title: '地域猫活動について',
-      date: '2023/04/01',
+      date: '2123/04/01',
       description: '地域猫活動についての説明です。',
       color: 'primary',
     },
     {
       title: '迷い猫情報【中央町】',
-      date: '2024/04/01',
+      date: '2124/04/01',
       description: '迷い猫の捜索協力をお願いします。',
       color: 'danger',
     },
     {
       title: '猫の飼い方セミナー',
-      date: '2025/04/05',
+      date: '2125/04/05',
       description: '猫の飼い方セミナーを開催します。市民センターにて。',
       color: 'success',
     },
     {
       title: 'サイトからのお知らせ',
-      date: '2026/04/01',
+      date: '2024/04/13',
       description: 'このサイトの掲載情報はすべてサンプルです。必要な情報は各自で管理アカウントでログインして追加してください。',
       color: 'warning',
     }
