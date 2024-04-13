d747dd5debe73db3fb35020826b5b0f3bac7317e
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-04-13 14:30:11 +0900

    feat: add dummy

--- cats/src/app/informations/informations.page.ts
+++ cats/src/app/informations/informations.page.ts
@@ -2,15 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-informations',
   templateUrl: './informations.page.html',
   styleUrls: ['./informations.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class InformationsPage implements OnInit {
+
+  informations = [
+    {
+      title: '地域猫活動について',
+      date: '2023/04/01',
+      description: '地域猫活動についての説明です。',
+      color: 'primary',
+    },
+    {
+      title: '迷い猫情報【中央町】',
+      date: '2024/04/01',
+      description: '迷い猫の捜索協力をお願いします。',
+      color: 'danger',
+    },
+    {
+      title: '猫の飼い方セミナー',
+      date: '2025/04/05',
+      description: '猫の飼い方セミナーを開催します。市民センターにて。',
+      color: 'success',
+    }
+  ]
 
   constructor() { }
 
